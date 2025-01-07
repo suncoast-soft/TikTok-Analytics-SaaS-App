4b25a7158fafa5502d6ec4979f7666f5cc65a955
Dewayne Johnson <dj@suncoast-software.com>
2025-01-07 12:21:46 -0500

    chore: seller card refactor

--- app/seller/[seller]/(public)/page.tsx
+++ app/seller/[seller]/(public)/page.tsx
@@ -1,12 +1,31 @@
+import { requestTikTokShopAPIClient } from '@/app/actions'
 import SellerCard from '@/components/sections/SellerCard'
 import { getUser } from '@/utils/supabase/queries'
 import { createClient } from '@/utils/supabase/server'
 
-type Params = Promise<{ seller: string }>
+const fetchSeller = async (seller: string | undefined) => {
+  const data = await requestTikTokShopAPIClient(
+    seller,
+    '/authorization/202309/shops',
+    {},
+    'GET',
+    ''
+  )
+  if (data.data && Array.isArray(data.data.shops)) {
+    return data.data.shops[0]
+  }
+  return null
+}
 
-export default async function SellerHomePage({ params }: { params: Params }) {
+export default async function SellerHomePage({
+  params
+}: {
+  params: Promise<{ seller: string }>
+}) {
   const supabase = await createClient()
   const user = await getUser(supabase)
 
-  return <SellerCard seller={(await params).seller} user={user} cta={true} />
+  const seller = await fetchSeller((await params).seller)
+
+  return <SellerCard seller={seller} user={user} cta={true} />
 }
--- components/sections/SellerCard/SellerCard.tsx
+++ components/sections/SellerCard/SellerCard.tsx
@@ -1,90 +1,43 @@
-'use client'
-
-import { requestTikTokShopAPIClient } from '@/app/actions'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import Loading from '@/components/modules/Loading'
 import StarRating from '@/components/modules/Rating'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
-
-const fetchSeller = async (seller: string | undefined) => {
-  const data = await requestTikTokShopAPIClient(
-    seller,
-    '/authorization/202309/shops',
-    {},
-    'GET',
-    ''
-  )
-  if (data.data && Array.isArray(data.data.shops)) {
-    return data.data.shops[0]
-  }
-  return null
-}
 
 export default function SellerHome({
   seller,
   user,
   cta
 }: {
-  seller: string | undefined
+  seller: any
   user: any
   cta?: boolean
 }) {
-  const [isLoading, setIsLoading] = useState(false)
-  const [sellerData, setSellerData] = useState<any>(null)
-
-  useEffect(() => {
-    const loadInitialData = async () => {
-      setIsLoading(true)
-
-      const data = await fetchSeller(seller)
-
-      setSellerData(data)
-      setIsLoading(false)
-    }
-
-    loadInitialData()
-  }, [seller])
-
-  if (isLoading) {
-    return (
-      <div className="container max-w-3xl py-20">
-        <Loading />
-      </div>
-    )
-  }
-
-  if (!sellerData) {
-    return <></>
-  }
-
   return (
     <div className="container max-w-3xl mx-auto py-20">
       <section className="bg-orange-50 px-4 py-8 rounded-lg shadow-lg transform hover:translate-y-1 transition flex flex-col md:flex-row gap-8 overflow-hidden">
         <Image
-          src={sellerData.image ?? '/images/temp/locked.jpeg'}
-          alt={sellerData.name}
+          src={seller.image ?? '/images/temp/locked.jpeg'}
+          alt={seller.name}
           width={200}
           height={200}
         />
 
         <div className="w-full">
-          <h1 className="text-2xl font-bold mb-4">{sellerData.name}</h1>
+          <h1 className="text-2xl font-bold mb-4">{seller.name}</h1>
 
           <p>
             <span>Shop Code: </span>
-            <strong>{sellerData.code}</strong>
+            <strong>{seller.code}</strong>
           </p>
 
           <p>
             <span>Region: </span>
-            <strong>{sellerData.region}</strong>
+            <strong>{seller.region}</strong>
           </p>
 
           <p>
             <span>Type: </span>
-            <strong>{sellerData.seller_type}</strong>
+            <strong>{seller.seller_type}</strong>
           </p>
 
           {cta && (
