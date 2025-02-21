b32aeed11cde579e4f1fdb484ca37ed2aae46567
Dewayne Johnson <dj@suncoast-software.com>
2025-02-21 13:38:20 -0500

    feat: terms and privacy pages

---
+++ app/(dashboard)/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import Card from '@/components/modules/Card';
+import Title from '@/components/modules/Title';
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="container max-w-7xl py-8">
+      <Card vertical={true}>
+        <Title title="Privacy Policy" />
+      </Card>
+    </div>
+  );
+}
---
+++ app/(dashboard)/terms-and-conditions/page.tsx
@@ -0,0 +1,12 @@
+import Card from '@/components/modules/Card';
+import Title from '@/components/modules/Title';
+
+export default function TermsAndConditions() {
+  return (
+    <div className="container max-w-7xl py-8">
+      <Card vertical={true}>
+        <Title title="Terms And Conditions" />
+      </Card>
+    </div>
+  );
+}
